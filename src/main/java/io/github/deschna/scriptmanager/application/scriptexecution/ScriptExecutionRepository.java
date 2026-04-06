47f16ad406b66eacc94c85ad1b8da83d664452c2
Pasha Ditkovskyi <pashkadit@gmail.com>
2026-04-06 21:05:17 +0300

    Add script execution repository contract and JPA adapter

---
+++ src/main/java/io/github/deschna/scriptmanager/application/scriptexecution/ScriptExecutionRepository.java
@@ -0,0 +1,12 @@
+package io.github.deschna.scriptmanager.application.scriptexecution;
+
+import io.github.deschna.scriptmanager.domain.scriptexecution.ScriptExecution;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ScriptExecutionRepository {
+
+    ScriptExecution save(ScriptExecution scriptExecution);
+
+    Optional<ScriptExecution> findById(UUID id);
+}
